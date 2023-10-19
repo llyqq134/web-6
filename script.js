dceae79688d794c69c56405b5191970d42402d1f
llyqq134 <pinklywmp@gmail.com>
2023-10-19 16:20:27 +0300

    done

--- script.js
+++ script.js
@@ -1,43 +1,60 @@
-function func() {
+function change() {
     let selectChoice = document.getElementsByName("product-choice")[0].value;
-    console.log(selectChoice);
-
+    if (selectChoice == 1) {
+        let checkBox = document.getElementById("option-choices");
+        checkBox.innerHTML = "";
+    }
     if (selectChoice == 2) {
         let checkBox = document.getElementById("option-choices");
         checkBox.innerHTML = '\
-        <input type="checkbox" name="scales">\n \
-        \t<label for="scales">scales</label>\n \
-        <input type="checkbox" name="horns">\n \
-        \t<label for="horns">horns</label> \
+        <input type="checkbox" id="onecomp">\n \
+        <label for="onecomp">однокомпонентное</label>\n \
+        <input type="checkbox" id="twocomp">\n \
+        <label for="twocomp">двухкомпонентное</label> \
         ';
     }
     else if (selectChoice == 3) {
         let checkBox = document.getElementById("option-choices");
         checkBox.innerHTML = '\
-        <input type="checkbox" name="scales">\n \
-        <label for="scales">scales</label>\n \
-        <input type="checkbox" name="horns">\n \
-        <label for="horns">horns</label>\n \
-        <input type="checkbox" name="fields">\n \
-        <label for="fields">fields</label> \
+        <input type="checkbox" id="acryl">\n \
+        <label for="acryl">акрил</label>\n \
+        <input type="checkbox" id="base">\n \
+        <label for="base">базовая</label>\n \
+        <input type="checkbox" id="powder">\n \
+        <label for="powder">порошковая</label> \
         ';
     }
+}
 
+function getSum() {
+    let selectChoice = document.getElementsByName("product-choice")[0].value;
     let sums = [100, 200, 400];
+    let resultSum = sums[selectChoice - 1] + checkOptions(selectChoice);
 
-    let resultSum = sums[selectChoice] + checkOptions(selectChoice);
-    let amount = document.getElementById("emount-of-products").value;
-    resultSum *= amount;
-
-    console.log(resultSum);
+    let amount = document.getElementById("amount-of-products").value;
+    if (amount.match(/^[0-9]$/))
+        resultSum *= amount;
+    else
+        alert("error");
     let result = document.getElementById("result");
+
+    result.innerHTML = resultSum;
 }
 
 function checkOptions(choice) {
     if (choice == 2) {
-        let checkbox1 = document.getElementsByName("scales");
-        let checkbox2 = document.getElementsByName("horns");
+        let checkbox1 = document.getElementById("onecomp");
+        let checkbox2 = document.getElementById("twocomp");
 
-        return checkbox1.checked * 20 + checkbox2.checked * 40;
+        return (checkbox1.checked ? 20 : 0) + (checkbox2.checked ? 40 : 0);
     }
+    else if (choice == 3) {
+        let checkbox1 = document.getElementById("acryl");
+        let checkbox2 = document.getElementById("base");
+        let checkBox3 = document.getElementById("powder");
+
+        return (checkbox1.checked ? 100 : 0) + (checkbox2.checked ? 200 : 0) + (checkBox3.checked ? 300 : 0);
+    }
+    else 
+        return 0;
 }
